8c4c6c1eecb6d068470fe7422168ca612c7f8700
Natalia Larina <koshka_208@yahoo.com>
2017-10-07 19:22:23 -0500

    fixed click handler to delete list items

--- public/app.js
+++ public/app.js
@@ -7,28 +7,36 @@ window.onload = function() {
     event.preventDefault();
     redWineList.push($('#redlist').val());
     $('#redwine').append("<li><input type='checkbox'>" + $('#redlist').val() + "</li>");
+    $('#redlist').val("");
+    addClickHandler();
   });
   $('#whitew').click(function(event){
     event.preventDefault();
     whiteWineList.push($('#whitelist').val());
     $('#whitewine').append("<li><input type='checkbox'>" + $('#whitelist').val() + "</li>");
+    $('#whitelist').val("");
+    addClickHandler();
   });
   $('#sparklingw').click(function(event){
     event.preventDefault();
     whiteWineList.push($('#sparklingist').val());
     $('#sparklingwine').append("<li><input type='checkbox'>" + $('#sparklinglist').val() + "</li>");
+    addClickHandler();
   });
   $('#rosew').click(function(event){
     event.preventDefault();
     whiteWineList.push($('#roselist').val());
     $('#rosewine').append("<li><input type='checkbox'>" + $('#roselist').val() + "</li>");
+    addClickHandler();
   });
 };
 
-$('li').click(function(event) {
-    var isChecked = $(this).children('input').prop('checked');
-    console.log(isChecked);
-    if (isChecked === true) {
-      $(this).remove();
-    }
-});
+function addClickHandler() {
+  $('li').click(function(event) {
+      var isChecked = $(this).children('input').prop('checked');
+      console.log(isChecked);
+      if (isChecked === true) {
+        $(this).remove();
+      }
+  });
+}
